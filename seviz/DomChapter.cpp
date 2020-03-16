9c036269b17d4d66c04e099174b51529d515a627
eltio <i.n.1998@inbox.ru>
2020-03-16 23:42:11 +0400

    fixed bugs in addStyleToSpan()

--- seviz/DomChapter.cpp
+++ seviz/DomChapter.cpp
@@ -28,43 +28,77 @@ void DomChapter::addStyleToSpan(const Position& from, const Position& to, const 
 	Position current(from);
 	current.setBook(m_chapter.book());
 
-	// начальные слова
-	do {
-		addStyle(current, css);
-		current = current.nextWord();
-	} while (current.hasNextWord() && current.nextWord() <= to);
-	// обработка предложений и абзацев посередине
-	// если from и to из одинаковых абзацев
-	if (from.paragraphId() == to.paragraphId()) {
-		for (int i = 0; i < (to.sentenceId() - from.sentenceId() - 1); ++i) {
+	// TODO refactor
+
+	assert(from.sectionId() != -1);
+	assert(from.chapterId() == to.chapterId());
+
+	if (from.wordId() != -1) {
+		// начальные слова
+		addStyle(from, css);
+		while (current.hasNextWord() && current < to) {
+			current = current.nextWord();
+			addStyle(current, css);
+		}
+		if (from.sentenceId() != to.sentenceId()) {
+			// обработка предложений и абзацев посередине
+		// если from и to из одинаковых абзацев
+			if (from.sentenceId() != to.sentenceId() && from.paragraphId() == to.paragraphId()) {
+				for (int i = 0; i < (to.sentenceId() - from.sentenceId() - 1); ++i) {
+					current = current.nextSentence();
+					addStyle(current, css);
+				}
+				current = current.nextSentence().firstWord();
+			} else {
+				// если из разных абзацев, то
+				// дообрабатываем предложения в абзаце из которого from
+				int idLastSentence = m_chapter.book()->getParagraph(from).size();
+				for (int i = 0; i < (idLastSentence - from.sentenceId()); ++i) {
+					current = current.nextSentence();
+					addStyle(current, css);
+				}
+				// обрабатываем абзацы посередине
+				for (int i = 0; i < (to.paragraphId() - from.paragraphId() - 1); ++i) {
+					current = current.nextParagraph();
+					addStyle(current, css);
+				}
+				current = current.nextParagraph().firstSentence();
+				// и начальные предложения абзаца to
+				for (int i = 0; i < to.sentenceId() - 1; ++i) {
+					addStyle(current, css);
+					current = current.nextSentence();
+				}
+				current = current.firstWord();
+			}
+		}
+		// конечные слова
+		while (current <= to) {
+			addStyle(current, css);
+			current = current.nextWord();
+		}
+	} else if (from.sentenceId() != -1) {
+		addStyle(from, css);
+		while (current.hasNextSentence() && current < to) {
 			current = current.nextSentence();
 			addStyle(current, css);
 		}
-		current = current.nextSentence().firstWord();
-	} else {
-		// если из разных абзацев, то
-		// дообрабатываем предложения в абзаце из которого from
-		int idLastSentence = m_chapter.book()->getParagraph(from).size();
-		for (int i = 0; i < (idLastSentence - from.sentenceId()); ++i) {
-			current = current.nextSentence();
-			addStyle(current, css);
-		}
-		// обрабатываем абзацы посередине
 		for (int i = 0; i < (to.paragraphId() - from.paragraphId() - 1); ++i) {
 			current = current.nextParagraph();
 			addStyle(current, css);
 		}
 		current = current.nextParagraph().firstSentence();
-		// и начальные предложения абзаца to
-		for (int i = 0; i < to.sentenceId() - 1; ++i) {
+		while (current < to) {
 			addStyle(current, css);
 			current = current.nextSentence();
 		}
-		current = current.firstWord();
+		addStyle(to, css);
+	} else if (from.paragraphId() != -1) {
+		addStyle(from, css);
+		while (current.hasNextParagraph() && current < to) {
+			current = current.nextParagraph();
+			addStyle(current, css);
+		}
+		addStyle(to, css);
 	}
-	// конечные слова
-	while (current <= to) {
-		addStyle(current, css);
-		current = current.nextWord();
-	} 
+	
 }
